60a9196bdf6849af4b8a550a9ad014d7c8a3cf1e
Erik Brink <erik.brink@gu.se>
2024-04-26 09:46:20 +0200

    Create parse_data.py

---
+++ backend/parse_data.py
@@ -0,0 +1,28 @@
+
+from parse import parse
+import csv
+from pandas import (
+    DataFrame, 
+    Series,
+    concat
+)
+
+def parse_kraken_output(path : str) -> DataFrame:
+    with open(path) as file:
+        reader = csv.reader(file, delimiter="\t", quotechar='"')
+        df = DataFrame()
+        for row in reader:
+            row[0] = row[0].strip()
+            row_dict = {
+                "Percentage of fragments covered by the clade rooted at this taxon" : [row[0]],
+                "Number of fragments covered by the clade rooted at this taxon" : [row[1]],
+                "Number of fragments assigned directly to this taxon" : [row[2]],
+                "Rank code" : [row[3]],
+                "NCBI taxonomic ID number" : [row[4]],
+                "Indented scientific name" : [row[5]]
+            }
+            
+            row_df = DataFrame.from_dict(row_dict)
+            
+            df = concat([df, row_df])
+    return df.reset_index().drop("index", axis=1)
