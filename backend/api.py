667a538ef974a97c9a134a472e18d463d66df9da
Erik Brink <erik.brink@gu.se>
2024-05-31 13:26:58 +0200

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -19,16 +19,16 @@ client = TestClient(app)
 db_h = DataBaseHandler("init-test-db", "data", ".secrets/credentials.env")
 
 @app.post("/retrieve_entries_by_filter/")
-def retrieve_entries_by_filter(data_base_request : RetrieveEntriesByFilter):
+def retrieve_entries_by_filter(data_base_request : RetrieveEntriesByFilter) -> list[dict]:
     entries = db_h.retrieve_entries_by_filter(data_base_request.filters)
     return list(entries)
 
 @app.post("/retrieve_entry_by_id/")
-def retrieve_entry_by_id(data_base_request : RetrieveEntryById):
+def retrieve_entry_by_id(data_base_request : RetrieveEntryById) -> list[dict]:
     entry = db_h.retrieve_entry_by_id(data_base_request.id)
     return list(entry)
 
 @app.post("/insert_entry/")
-def insert_entry(data_base_request : InsertEntries):
+def insert_entry(data_base_request : InsertEntries) -> None:
     db_h.insert_entry(data_base_request.entry)
     
