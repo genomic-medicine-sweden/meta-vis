c6bcdbe7595f81602209b7198b835ba808b947c8
Erik Brink <erik.brink@gu.se>
2024-06-10 14:07:28 +0200

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -17,15 +17,26 @@ db_h = DataBaseHandler("init-test-db", "data", ".secrets/credentials.env")
 
 @app.post("/retrieve_entries_by_filter/")
 def retrieve_entries_by_filter(data_base_request : RetrieveEntriesByFilter) -> list[dict]:
+    """
+    Retrieve database entries by a filter (specified as a dictionary) that 
+    MongoDB accepts. For more information about how to structure the queries follow this link:
+    https://pymongo.readthedocs.io/en/stable/tutorial.html#querying-for-more-than-one-document.
+    """
     entries = db_h.retrieve_entries_by_filter(data_base_request.filters)
     return list(entries)
 
 @app.get("/retrieve_entry_by_id/{id}")
 def retrieve_entry_by_id(id : str) -> list[dict]:
+    """
+    Retrieve a database entry by its ID.
+    """
     entry = db_h.retrieve_entry_by_id(id)
     return list(entry)
 
 @app.post("/insert_entry/")
 def insert_entry(data_base_request : InsertEntries) -> None:
+    """
+    Insert an entry to the database, which is in the shape of a JSON-object. 
+    """
     db_h.insert_entry(data_base_request.entry)
     
