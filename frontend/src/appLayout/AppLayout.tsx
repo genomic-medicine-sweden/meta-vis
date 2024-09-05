67d7a7e30230ee85c958089172299bf5892d1ede
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2024-09-05 15:13:36 +0200

    Add appMenuItems styles

--- frontend/src/appLayout/AppLayout.tsx
+++ frontend/src/appLayout/AppLayout.tsx
@@ -1,21 +1,24 @@
 import { Layout, Menu } from 'antd';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import './AppLayout.css';
 
 const { Header, Content, Footer } = Layout;
 
 const appMenuItems = () => [
     {
-      key: 'home',
+      key: '/',
       label: <Link to="/">Home</Link>,
     },
     {
-      key: 'about',
+      key: '/about',
       label: <Link to="/about">About</Link>,
     }
   ];
 
-export const AppLayout = () => {
-    const menuItems = appMenuItems();
+  
+  export const AppLayout = () => {
+      const menuItems = appMenuItems();
+      const location = useLocation();
     return (
         <Layout>
             <Header
@@ -29,13 +32,14 @@ export const AppLayout = () => {
                 }}
             >
                 <div className="logo">
-                    <img src="/meta-vis-logo.svg" alt="logo" style={{ height: '40px' }} />
+                        <img src="/meta-vis-logo.svg" alt="logo"/>            
                 </div>
                 <Menu
                     theme="dark"
                     mode="horizontal"
                     items={menuItems}
                     style={{ flex: 1, minWidth: 0 }}
+                    defaultSelectedKeys={[location.pathname]}
                 />
             </Header>
             <Content style={{ padding: '0 48px' }}>
