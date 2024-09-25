16d0ced9c0d39863cbf4ae1b8af751bc19347f03
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2024-09-25 14:09:03 +0200

    Add Metadata page

---
+++ frontend/src/pages/Metadata.tsx
@@ -0,0 +1,10 @@
+export const Metadata = () => {
+  return (
+    <>
+    <h2>Metadata</h2>
+   <li> A table including sample information, sample source, batch, ticket number, priority, accreditation, library preparation, CT values, wet-lab methods, Taxprofiler version, and software versions.</li>
+   <li>A brief description, a few sentences, of the taxprofiler pipeline analysis used to produce the results.</li>
+   <li> A summary table of tickets, sample information, key QC metrics, pass or fail status (?), and the number of reads assigned to viruses.</li>
+   </>
+  )
+}
