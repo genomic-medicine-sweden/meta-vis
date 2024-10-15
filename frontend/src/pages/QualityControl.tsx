5857fa7f585a565e09b3fa80314a4055c52ea949
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2024-10-15 09:56:17 +0200

    Create mock folder

--- frontend/src/pages/QualityControl.tsx
+++ frontend/src/pages/QualityControl.tsx
@@ -1,73 +1,11 @@
 import { Button, message, Table } from "antd"
 import { DownloadOutlined } from '@ant-design/icons'; 
+import { qualityControlData } from "../mock/qualityControlData";
 
 
 export const QualityControl = () => {
 
-  const data = [
-    {
-      key: '1',
-      totalReads: 180000,
-      totalReadsAfterFiltering: 175000,
-      humanMappedReads: 85000,
-      humanKrakenReads: 5000,
-      totalHumanReadsPercent: '48.57%', 
-      bacterialReadsPercent: '22.86%',  
-      virusReadsPercent: '1.71%',     
-      eukaryoteReadsPercent: '4%',      
-      otherReadsPercent: '1.71%',       
-      unclassifiedReadsPercent: '4%',  
-      spikeReadsPercent: '2.29%',       
-      spike: 'Emesvirus zinderi',
-    },
-    {
-      key: '2',
-      totalReads: 150000,
-      totalReadsAfterFiltering: 140000,
-      humanMappedReads: 60000,
-      humanKrakenReads: 4000,
-      totalHumanReadsPercent: '45.71%',
-      bacterialReadsPercent: '25.71%', 
-      virusReadsPercent: '2.14%',      
-      eukaryoteReadsPercent: '5%',    
-      otherReadsPercent: '2.14%',       
-      unclassifiedReadsPercent: '2.86%',
-      spikeReadsPercent: '1.43%',     
-      spike: 'Emesvirus zinderi',
-    },
-    {
-      key: '3',
-      totalReads: 200000,
-      totalReadsAfterFiltering: 190000,
-      humanMappedReads: 90000,
-      humanKrakenReads: 7000,
-      totalHumanReadsPercent: '50.53%',
-      bacterialReadsPercent: '26.32%', 
-      virusReadsPercent: '3.16%',       
-      eukaryoteReadsPercent: '4.74%',   
-      otherReadsPercent: '1.58%',       
-      unclassifiedReadsPercent: '2.11%',
-      spikeReadsPercent: '1.58%',       
-      spike: 'Emesvirus zinderi',
-    },
-    {
-      key: '4',
-      totalReads: 160000,
-      totalReadsAfterFiltering: 155000,
-      humanMappedReads: 70000,
-      humanKrakenReads: 4000,
-      totalHumanReadsPercent: '47.74%', 
-      bacterialReadsPercent: '22.58%',  
-      virusReadsPercent: '1.94%',       
-      eukaryoteReadsPercent: '3.87%',   
-      otherReadsPercent: '1.94%',      
-      unclassifiedReadsPercent: '3.23%',
-      spikeReadsPercent: '2.58%',       
-      spike: 'Emesvirus zinderi',
-    },
-  ];
   
-
   
   const columns = [
     {
@@ -144,6 +82,6 @@ export const QualityControl = () => {
 
 
   return (
-    <Table dataSource={data} columns={columns} bordered/>
+    <Table dataSource={qualityControlData} columns={columns} bordered/>
   )
 }
